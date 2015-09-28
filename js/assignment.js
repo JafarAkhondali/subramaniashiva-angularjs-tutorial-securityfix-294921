4f2fff40287657a3f0d60099586ae69773086c01
subramaniashiva <vengaishiva@gmail.com>
2015-09-28 22:16:55 +0530

    Updated assignment file with directives and services

--- js/assignment.js
+++ js/assignment.js
@@ -1,46 +1,79 @@
 'use strict';
-var myApp = angular.module('myApp',[]);
-myApp.factory('block', function() {
-	var block = {};
-	block.top = [];
-	block.addTopBlock = function(name, age) {
-		var obj = {};
-		obj.name = name;
-		obj.age = age;
-		block.top.push(obj);
-	}
-	return block;
+var myApp = angular.module('myApp', []);
+myApp.factory('notes', function() {
+    var notes = {};
+    notes.officeNotes = [];
+    notes.addOfficeNotes = function(task, priority) {
+        var obj = {};
+        obj.task = task;
+        obj.priority = priority;
+        obj.id = notes.officeNotes.length;
+        notes.officeNotes.push(obj);
+    }
+    notes.personalNotes = [];
+    notes.addPersonalNotes = function(task, priority, length, row) {
+        var obj = {};
+        obj.task = task;
+        obj.priority = priority;
+        obj.id = notes.personalNotes.length + 1;
+        obj.length = length;
+        obj.row = row;
+        notes.personalNotes.push(obj);
+    }
+    return notes;
 });
-myApp.controller('mainController', function(block) {
-	var self = this;
-	this.topBlocks = block.top;
-	this.bottomBlocks = [];
-	this.name = '';
-	this.age = '';
-	this.addTopBlock = function(isValid) {
-		if(isValid) {
-			block.addTopBlock(this.name, this.age);
-			this.form.$setPristine();
-			// Since Angular 1.3, set back to untouched state.
-			this.form.$setUntouched();
-
-			this.name = '';
-			this.age = '';
-		}
-	}
+myApp.controller('mainController', function(notes) {
+    var self = this;
+    this.officeNotes = notes.officeNotes;
+    this.personalNotes = notes.personalNotes;
+    this.officeTask = '';
+    this.officePriority = '';
+    this.personalTask = '';
+    this.personalPriority = '';
+    this.personalTaskLength = 0;
+    this.personalRow = 0;
+    this.prioritiesList = ['High', 'Medium', 'Low'];
+    this.addNotes = function(type, form) {
+        if (form.$valid) {
+            if (type === 'office') {
+                notes.addOfficeNotes(this.officeTask, this.officePriority);
+                this.officeTask = '';
+                this.officePriority = '';
+            } else {
+                if (this.personalTaskLength > 5) {
+                    this.personalRow++;
+                    this.personalTaskLength = 1;
+                } else {
+                    this.personalTaskLength++;
+                }
+                notes.addPersonalNotes(this.personalTask, this.personalPriority, this.personalTaskLength, this.personalRow);
+                this.personalTask = '';
+                this.personalPriority = '';
+            }
+            form.$setPristine();
+            form.$setUntouched();
+        }
+    }
 });
-myApp.directive('topblock', function() {
-	return {
-		restrict: 'E',
-		transclude: true,
-		template: '<div class="pull-left top"><ng-transclude></ng-transclude></div>'
-	}
+myApp.directive('sticknote', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            task: '@',
+            priority: '@',
+            id: '@',
+            len: '@',
+            row: '@'
+        },
+        template: '<div class="note" style="right: {{ 172 * (len-1) }}px; bottom: {{ 122*row }}px"><p>{{ task }}</p><p>{{ priority }}</p><ng-transclude></ng-transclude></div>'
+    }
 });
 
 myApp.directive('leaving', function() {
-	return function(scope, element, attrs) {
-		element.bind('mouseleave', function() {
-			element.removeClass(attrs.entering);
-		});		
-	}
+    return function(scope, element, attrs) {
+        element.bind('mouseleave', function() {
+            element.removeClass(attrs.entering);
+        });
+    }
 });
